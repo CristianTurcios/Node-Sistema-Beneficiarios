0a1a03fc8ea0e24f8b2256c25bd1f01fb383be94
Cristian Turcios <cturcioscolindres01@gmail.com>
2023-10-10 12:01:53 -0600

    added new endpoint to export csv data

--- becas/becas.controller.js
+++ becas/becas.controller.js
@@ -1,126 +1,140 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Joi = require('joi');
-const validateRequest = require('_middleware/validate-request');
-const authorize = require('_middleware/authorize')
-const becasService = require('./becas.service');
+const Joi = require("joi");
+const validateRequest = require("_middleware/validate-request");
+const authorize = require("_middleware/authorize");
+const becasService = require("./becas.service");
+var { Parser } = require("json2csv");
 
 // routes
-router.post('/', authorize(), registerSchema, register);
-router.get('/', authorize(), getAll);
-router.get('/:id', authorize(), getById);
-router.put('/:id', authorize(), updateSchema, update);
-router.delete('/:id', authorize(), _delete);
+router.post("/", authorize(), registerSchema, register);
+router.get("/", authorize(), getAll);
+router.get("/report", authorize(), getReport);
+router.get("/:id", authorize(), getById);
+router.put("/:id", authorize(), updateSchema, update);
+router.delete("/:id", authorize(), _delete);
 
 module.exports = router;
 
 function registerSchema(req, res, next) {
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        lastName: Joi.string().required(),
-        identidad: Joi.string().min(13).required(),
-        sex: Joi.string().required(),
-        bornDate: Joi.string().required(),
-        age: Joi.number().required(),
-        department: Joi.string().required().allow(''),
-        municipality: Joi.string().required().allow(''),
-        village: Joi.string().required().allow(''),
-        barrio: Joi.string().required().allow(''),
-        currentAddress: Joi.string().required(),
-        phone: Joi.string().min(8).required(),
-        email: Joi.string().optional().email(),
-        // *** General information
-        ethnicity: Joi.string().required(),
-        vulnerableGroup: Joi.any().required(),
-        civilStatus: Joi.string().required(),
-        numberOfChildren: Joi.number().optional(),
-        // *** Institution Information  ****
-        educationalCenterName: Joi.string().required(),
-        educationalCenterAddress: Joi.string().required(),
-        academicLevel: Joi.string().required(),
-        grade: Joi.number().required(),
-        // *** Fathers or tutors information ***
-        motherOrFatherName: Joi.string().optional().allow(''),
-        motherOrFatherLastName: Joi.string().optional().allow(''),
-        numberOfChildrenFathers: Joi.number().optional(),
-        educationalLevelMotherOrFather: Joi.string().optional().allow(''),
-        identidadMotherOrFather: Joi.string().min(13).optional().allow(''),
-        fathersAddress: Joi.string().optional().allow(''),
-        motherOrFatherWork: Joi.string().optional().allow(''),
-        monthlyIncome: Joi.string().optional(),
-        fathersPhone: Joi.string().optional().allow(''),
-        observation: Joi.string().optional().allow(''),
-    });
-    validateRequest(req, next, schema);
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    lastName: Joi.string().required(),
+    identidad: Joi.string().min(13).required(),
+    sex: Joi.string().required(),
+    bornDate: Joi.string().required(),
+    age: Joi.number().required(),
+    department: Joi.string().required().allow(""),
+    municipality: Joi.string().required().allow(""),
+    village: Joi.string().required().allow(""),
+    barrio: Joi.string().required().allow(""),
+    currentAddress: Joi.string().required(),
+    phone: Joi.string().min(8).required(),
+    email: Joi.string().optional().email(),
+    // *** General information
+    ethnicity: Joi.string().required(),
+    vulnerableGroup: Joi.any().required(),
+    civilStatus: Joi.string().required(),
+    numberOfChildren: Joi.number().optional(),
+    // *** Institution Information  ****
+    educationalCenterName: Joi.string().required(),
+    educationalCenterAddress: Joi.string().required(),
+    academicLevel: Joi.string().required(),
+    grade: Joi.number().required(),
+    // *** Fathers or tutors information ***
+    motherOrFatherName: Joi.string().optional().allow(""),
+    motherOrFatherLastName: Joi.string().optional().allow(""),
+    numberOfChildrenFathers: Joi.number().optional(),
+    educationalLevelMotherOrFather: Joi.string().optional().allow(""),
+    identidadMotherOrFather: Joi.string().min(13).optional().allow(""),
+    fathersAddress: Joi.string().optional().allow(""),
+    motherOrFatherWork: Joi.string().optional().allow(""),
+    monthlyIncome: Joi.string().optional(),
+    fathersPhone: Joi.string().optional().allow(""),
+    observation: Joi.string().optional().allow(""),
+  });
+  validateRequest(req, next, schema);
 }
 
 function register(req, res, next) {
-    becasService.create(req.body)
-        .then(() => res.json({ message: 'Registration successful' }))
-        .catch(next);
+  becasService
+    .create(req.body)
+    .then(() => res.json({ message: "Registration successful" }))
+    .catch(next);
 }
 
 function getAll(req, res, next) {
-    const { id, page, size } = req.query;
-    becasService.getAll(id, page, size)
-        .then(data => res.json(data))
-        .catch(next);
+  const { id, page, size } = req.query;
+  becasService
+    .getAll(id, page, size)
+    .then((data) => res.json(data))
+    .catch(next);
+}
+
+function getReport(req, res, next) {
+    becasService
+      .getReport()
+      .then((data) => res.json(data))
+      .catch(next);
 }
 
 function getById(req, res, next) {
-    becasService.getById(req.params.id)
-        .then(data => res.json(data))
-        .catch(next);
+  becasService
+    .getById(req.params.id)
+    .then((data) => res.json(data))
+    .catch(next);
 }
 
 function updateSchema(req, res, next) {
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        lastName: Joi.string().required(),
-        identidad: Joi.string().min(13).required(),
-        sex: Joi.string().required(),
-        bornDate: Joi.string().required(),
-        age: Joi.number().required(),
-        department: Joi.string().required().allow(''),
-        municipality: Joi.string().required().allow(''),
-        village: Joi.string().required().allow(''),
-        barrio: Joi.string().required().allow(''),
-        currentAddress: Joi.string().required(),
-        phone: Joi.string().min(8).required(),
-        email: Joi.string().optional().email(),
-        // *** General information
-        ethnicity: Joi.string().required(),
-        vulnerableGroup: Joi.any().required(),
-        civilStatus: Joi.string().required(),
-        numberOfChildren: Joi.number().optional(),
-        // *** Institution Information  ****
-        educationalCenterName: Joi.string().required(),
-        educationalCenterAddress: Joi.string().required(),
-        academicLevel: Joi.string().required(),
-        grade: Joi.number().required(),
-        // *** Fathers or tutors information ***
-        motherOrFatherName: Joi.string().optional().allow(''),
-        motherOrFatherLastName: Joi.string().optional().allow(''),
-        numberOfChildrenFathers: Joi.number().optional(),
-        educationalLevelMotherOrFather: Joi.string().optional().allow(''),
-        identidadMotherOrFather: Joi.string().min(13).optional().allow(''),
-        fathersAddress: Joi.string().optional().allow(''),
-        motherOrFatherWork: Joi.string().optional().allow(''),
-        monthlyIncome: Joi.string().optional(),
-        fathersPhone: Joi.string().optional().allow(''),
-        observation: Joi.string().optional().allow(''),
-    });
-    validateRequest(req, next, schema);
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    lastName: Joi.string().required(),
+    identidad: Joi.string().min(13).required(),
+    sex: Joi.string().required(),
+    bornDate: Joi.string().required(),
+    age: Joi.number().required(),
+    department: Joi.string().required().allow(""),
+    municipality: Joi.string().required().allow(""),
+    village: Joi.string().required().allow(""),
+    barrio: Joi.string().required().allow(""),
+    currentAddress: Joi.string().required(),
+    phone: Joi.string().min(8).required(),
+    email: Joi.string().optional().email(),
+    // *** General information
+    ethnicity: Joi.string().required(),
+    vulnerableGroup: Joi.any().required(),
+    civilStatus: Joi.string().required(),
+    numberOfChildren: Joi.number().optional(),
+    // *** Institution Information  ****
+    educationalCenterName: Joi.string().required(),
+    educationalCenterAddress: Joi.string().required(),
+    academicLevel: Joi.string().required(),
+    grade: Joi.number().required(),
+    // *** Fathers or tutors information ***
+    motherOrFatherName: Joi.string().optional().allow(""),
+    motherOrFatherLastName: Joi.string().optional().allow(""),
+    numberOfChildrenFathers: Joi.number().optional(),
+    educationalLevelMotherOrFather: Joi.string().optional().allow(""),
+    identidadMotherOrFather: Joi.string().min(13).optional().allow(""),
+    fathersAddress: Joi.string().optional().allow(""),
+    motherOrFatherWork: Joi.string().optional().allow(""),
+    monthlyIncome: Joi.string().optional(),
+    fathersPhone: Joi.string().optional().allow(""),
+    observation: Joi.string().optional().allow(""),
+  });
+  validateRequest(req, next, schema);
 }
 
 function update(req, res, next) {
-    becasService.update(req.params.id, req.body)
-        .then(user => res.json(user))
-        .catch(next);
+  becasService
+    .update(req.params.id, req.body)
+    .then((user) => res.json(user))
+    .catch(next);
 }
 
 function _delete(req, res, next) {
-    becasService.delete(req.params.id)
-        .then(() => res.json({ message: 'Registro Borrado con exito' }))
-        .catch(next);
+  becasService
+    .delete(req.params.id)
+    .then(() => res.json({ message: "Registro Borrado con exito" }))
+    .catch(next);
 }
--- becas/becas.service.js
+++ becas/becas.service.js
@@ -1,21 +1,22 @@
-const db = require('_helpers/db');
+const db = require("_helpers/db");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 const { getPagination, getPagingData } = require("_helpers/pagination");
 
 module.exports = {
-    getAll,
-    getById,
-    create,
-    update,
-    delete: _delete
+  getAll,
+  getReport,
+  getById,
+  create,
+  update,
+  delete: _delete,
 };
 
 async function getAll(id, page, size) {
   const { limit, offset } = getPagination(page, size);
 
   const condition = id ? { identidad: { [Op.like]: `%${id}%` } } : null;
-  
+
   const data = await db.Becas.findAndCountAll({
     where: condition,
     limit,
@@ -25,50 +26,58 @@ async function getAll(id, page, size) {
   return getPagingData(data, page, limit);
 }
 
+async function getReport() {
+    return await db.Becas.findAll();
+}
+
 async function getById(id) {
-    return await getRegistro(id);
+  return await getRegistro(id);
 }
 
 async function create(params) {
-    // validate
-    if (await db.Becas.findOne({ where: { identidad: params.identidad } })) {
-        throw 'Identidad "' + params.identidad + '" ya existe en el sistema';
-    }
+  // validate
+  if (await db.Becas.findOne({ where: { identidad: params.identidad } })) {
+    throw 'Identidad "' + params.identidad + '" ya existe en el sistema';
+  }
 
-    if (params.grade < 75) {
-        throw 'Indice Academico debe ser igual o mayor a 75%';
-    }
+  if (params.grade < 75) {
+    throw "Indice Academico debe ser igual o mayor a 75%";
+  }
 
-    // save user
-    await db.Becas.create(params);
+  // save user
+  await db.Becas.create(params);
 }
 
 async function update(id, params) {
-    const becas = await getRegistro(id);
+  const becas = await getRegistro(id);
 
-    // validate
-    const identidadChanged = params.identidad && becas.identidad !== params.identidad;
-    if (identidadChanged && await db.Becas.findOne({ where: { identidad: params.identidad } })) {
-        throw 'Identidad "' + params.identidad + '" ya existe en el sistema';
-    }
+  // validate
+  const identidadChanged =
+    params.identidad && becas.identidad !== params.identidad;
+  if (
+    identidadChanged &&
+    (await db.Becas.findOne({ where: { identidad: params.identidad } }))
+  ) {
+    throw 'Identidad "' + params.identidad + '" ya existe en el sistema';
+  }
 
-    if (params.grade < 75) {
-        throw 'Indice Academico debe ser igual o mayor a 75%';
-    }
+  if (params.grade < 75) {
+    throw "Indice Academico debe ser igual o mayor a 75%";
+  }
 
-    // copy params to user and save
-    await db.Becas.update(params, { where: { id } });
-    return await getRegistro(id);
+  // copy params to user and save
+  await db.Becas.update(params, { where: { id } });
+  return await getRegistro(id);
 }
 
 async function _delete(id) {
-    const becas = await getRegistro(id);
-    await becas.destroy();
+  const becas = await getRegistro(id);
+  await becas.destroy();
 }
 
 // helper functions
 async function getRegistro(id) {
-    const beca = await db.Becas.findByPk(id);
-    if (!beca) throw 'Registro no encontrado';
-    return beca;
+  const beca = await db.Becas.findByPk(id);
+  if (!beca) throw "Registro no encontrado";
+  return beca;
 }
